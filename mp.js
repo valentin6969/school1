f67e342b6963a2fac717cbd5e6d53dcffe762448
valentin6969 <49337613+valentin6969@users.noreply.github.com>
2019-05-25 12:38:07 +0300

    Update mp.js

--- mp.js
+++ mp.js
@@ -1,9 +1,22 @@
 $(document).ready(() => {
-$('.menu-button').on('mouseenter', () => {
-    $('.nav-menu').show()
-  })
+    $('.menu-button').on('mouseenter', () => {
+        $('.nav-menu').show();
+        $('.menu-button').addClass('button-active');
+        $('.nav-menu').removeClass('hide');
+        
+        $('.menu-button').animate({
+          fontSize: '24px'
+        }, 200)
+      })
   
   $('.nav-menu').on('mouseleave', () => {
     $('.nav-menu').hide();
+  })
+  $('.nav-menu').on('mouseleave', () => {
+    $('.menu-button').removeClass('button-active')
+    
+    $('.menu-button').animate({
+      fontSize: '18px'
+    }, 200)
   })
 }); 
