7de3ba4ab627854a1bb01feef03aef9ce1a3f14c
valentin6969 <49337613+valentin6969@users.noreply.github.com>
2019-05-25 12:26:16 +0300

    Add files via upload

---
+++ mp.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+$('.menu-button').on('mouseenter', () => {
+    $('.nav-menu').show()
+  })
+  
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+  })
+}); 
